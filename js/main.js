15a548be2e0ecfc854a13ebcd9566de46cbdaf54
ardiankurniawan8 <ardian.smbc@gmail.com>
2021-03-10 00:59:03 +0700

    update

--- js/main.js
+++ js/main.js
@@ -304,13 +304,6 @@
             },
         });
 
-        //Google Maps
-        $("#map").googleMap({
-            zoom: 16 // Google Map ZOOM. You can change this value
-        });
-        $("#map").addMarker({
-            address: "S601 Townsend Street, San Francisco, California, USA", // Your Address. Change it
-        });
     });
 
 })(jQuery);
